4cd8e11535e5d02c7232df21c225e9e8ce46fb99
Ryan Schuhler <ryan.schuhler@liferay.com>
2024-10-22 11:27:04 -0700

    LRSD-6284 [LRSD-6618] Components - Fix type errors

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/common/utils/getToast.ts
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/common/utils/getToast.ts
@@ -3,8 +3,15 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 import i18n from '../I18n';
+import {Liferay} from '../services/liferay';
 
-const openToast = (title, message, {type = 'success'} = {}) =>
+interface IProps {
+	message: string;
+	title: string;
+	type?: 'success' | 'danger' | 'info' | 'warning';
+}
+
+const openToast = ({message, title, type = 'success'}: IProps) =>
 	Liferay.Util.openToast({
 		message: i18n.translate(message),
 		title: i18n.translate(title),
